4435b224a5c88bc11d22649197ec015ff8157879
ryanfok <ryanfok@cs.washington.edu>
2019-11-02 20:47:02 -0700

    created files

---
+++ src/components/InOutButton.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InOutButton extends Component {
+    render() {
+        return (
+            <div>
+                In/Out Button
+            </div>
+        );
+    }
+}
+
+export default InOutButton;
