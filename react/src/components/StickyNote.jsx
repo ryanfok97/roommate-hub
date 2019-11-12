18bc2804947d99916077615e5ff48e4cda3edcc8
ryanfok <ryanfok@cs.washington.edu>
2019-11-11 23:45:39 -0800

    editable note title and text with styling...probably will need to add it to state in future? tbd

--- react/src/components/StickyNote.jsx
+++ react/src/components/StickyNote.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Form } from 'react-bootstrap';
 
 class StickyNote extends Component {
     render() {
         /* TODO:
-            * resizable
-            * editable
-            * bold/italic/underline/strikethrough capability
-            * list capability
-            * lil x button to delete
-            * titles
+            * resizable (entire sticky note)
+            * editable (title, text both optional and editable)
+            * bold/italic/underline/strikethrough capability (for text)
+            * list capability (for text)
+            * lil x button to delete (top right corner)
+            * titles (optional and editable)
             * movable
         */
         return (
             <Card className='sticky-note'>
-                <Card.Title>title</Card.Title>
-                <Card.Body>
-                    <Card.Text>
-                        a sticky note! wow!
+                <Card.Body style={{padding: '.25rem'}}>
+                    <Card.Title as='input' className='sticky-note-title h5' placeholder='Note Title'></Card.Title>
+                    <Card.Text as='textarea' className='sticky-note-text' placeholder='Note Text'>
                     </Card.Text>
                 </Card.Body>
             </Card>
