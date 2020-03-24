9c71cc1061f43a955021f7b1d96161f19371d728
Brandon Fok <fokb@cs.washington.edu>
2020-03-23 17:27:39 -0700

    clear old files

--- api/routes/calendar-api-routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var calendarController = require('../controllers/calendar-api-controller');
-
-router.get('/events', (req, res) => {
-    calendarController.listEvents(res);
-});
-
-module.exports = router;
